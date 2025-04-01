3fa629ecfc37825e04c1675727df093ed34b7868
Łukasz Balewicz <251121@edu.p.lodz.pl>
2025-04-01 14:21:31 +0200

    6.1 deklaracja stałych/zmiana literałów

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -10,7 +10,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     @Override
     public void countIn(int in) {
-        if (!callCheck() && in > peekaboo())
+        if (!callCheck() && in > peekAtTop())
             totalRejected++;
         else
             super.countIn(in);
