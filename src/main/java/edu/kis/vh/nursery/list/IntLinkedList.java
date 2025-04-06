589843a1adab09709926f3d9a1c9aa5772c0d632
PrzemyslawZych <252843@edu.p.lodz.pl>
2025-04-06 14:16:05 +0200

    Punkt 14: Przeniesiono klasę Node jako klasę wewnętrzną do IntLinkedList, usunięto osobny plik

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -36,5 +36,32 @@ public class IntLinkedList {
         last = last.prev;
         return ret;
     }
+    private static class Node {
+        private final int value;
+        private Node prev, next;
 
+        public Node(int i) {
+            value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
